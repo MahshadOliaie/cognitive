bdf0f0b75fc91047e91460a1191051dfc7c20771
MahshadOliaie <mahshadoliaie@gmail.com>
2024-08-11 23:24:50 +0330

    edit

--- src/components/Buttons/AddBtn.jsx
+++ src/components/Buttons/AddBtn.jsx
@@ -1,10 +1,10 @@
-
+import plus from '/plus.svg'
 
 
 function AddBtn(){
     return(
         <div className="w-12 h-12 p-3 flex items-center justify-center rounded-full shadow-lg cursor-pointer hover:scale-105 duration-150 bg-dark">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="white" viewBox="0 0 448 512"><path d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 144L48 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l144 0 0 144c0 17.7 14.3 32 32 32s32-14.3 32-32l0-144 144 0c17.7 0 32-14.3 32-32s-14.3-32-32-32l-144 0 0-144z"/></svg>
+            <img src={plus} alt="" />
         </div>
     )
 }
