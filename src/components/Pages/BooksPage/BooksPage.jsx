f7bac74aafa7d8bc57119013a7f6e59bf5ffa123
MahshadOliaie <mahshadoliaie@gmail.com>
2024-08-11 23:33:17 +0330

    add search to books page

--- src/components/Pages/BooksPage/BooksPage.jsx
+++ src/components/Pages/BooksPage/BooksPage.jsx
@@ -1,13 +1,16 @@
 import { useEffect, useState } from "react"
 import BookCard from "../../BookCard/BookCard"
 import useFetch from "../../../hooks/useFetch"
+import CustomSearchBar from "../../CustomSearchBar/CustomSearchBar"
 
 function BooksPage() {
     const [data, setData] = useState([])
+    const [filteredData, setFilteredData] = useState([])
     const books = useFetch('/books.json')
 
     useEffect(() => {
         setData(books)
+        setFilteredData(books)
 
         return () => {
 
@@ -16,11 +19,16 @@ function BooksPage() {
     }, [books])
 
     return (
-        <div className="flex flex-col gap-6">
-            {data.map(book => {
-                return <BookCard key={book.id} data={book} />
-            })}
-        </div>
+        <>
+            <div className="flex items-center justify-between px-4">
+                <CustomSearchBar setFilteredData={setFilteredData} data={data}/>
+            </div>
+            <div className="flex flex-col gap-6 py-7">
+                {filteredData.map(book => {
+                    return <BookCard key={book.id} data={book} />
+                })}
+            </div>
+        </>
     )
 }
 
