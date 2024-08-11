e6cd077019ede02269ad1535ea686c968e226494
MahshadOliaie <mahshadoliaie@gmail.com>
2024-08-11 17:03:37 +0330

    add publush state

--- src/components/Pages/AddBookPage/AddBookPage.jsx
+++ src/components/Pages/AddBookPage/AddBookPage.jsx
@@ -48,6 +48,15 @@ function AddBookPage() {
             cell: (props) => <p>{props.getValue().name}</p>
         },
         {
+            accessorKey: "publish",
+            header: "انتشار",
+            size: 60,
+            cell: (props) => (props.getValue()) ?
+                <svg className="w-5 m-auto" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="#77bb41" d="M438.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L160 338.7 393.4 105.4c12.5-12.5 32.8-12.5 45.3 0z" /></svg>
+                :
+                <svg className="w-5 m-auto" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path fill="#e32400" d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z" /></svg>
+        },
+        {
             accessorKey: "publicationYear",
             header: "سال انتشار",
             cell: (props) => <p>{props.getValue()}</p>
@@ -55,7 +64,7 @@ function AddBookPage() {
         {
             accessorKey: "pageNumber",
             header: "صفحات",
-            size: 100,
+            size: 90,
             cell: (props) => <p>{props.getValue()}</p>
         },
         {
