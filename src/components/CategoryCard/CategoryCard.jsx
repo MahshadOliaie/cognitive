d7f655a515c2ecb25922a9d48e7df9fe991fc70c
MahshadOliaie <mahshadoliaie@gmail.com>
2024-08-11 23:41:22 +0330

    edit

--- src/components/CategoryCard/CategoryCard.jsx
+++ src/components/CategoryCard/CategoryCard.jsx
@@ -1,6 +1,9 @@
 import { useState } from "react"
 import DeleteBtn from "../Buttons/DeleteBtn"
 import EditBtn from "../Buttons/EditBtn"
+import down from '/arrowDown.svg'
+import up from '/arrowUp.svg'
+
 
 
 function CategoryCard({ category }) {
@@ -22,11 +25,11 @@ function CategoryCard({ category }) {
                 {(children.length > 0) ?
                     (openSub) ?
                         <div className="moreBtn w-10 h-10 flex items-center justify-center absolute -bottom-4 shadow-md p-3 rounded-full bg-sandals cursor-pointer hover:scale-105 duration-150" style={{ left: "calc(50% - 1.25rem)" }} onClick={handleOpen}>
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="white" viewBox="0 0 448 512" style={{ rotate: "180deg", transition: "300ms" }}><path d="M201.4 374.6c12.5 12.5 32.8 12.5 45.3 0l160-160c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 306.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l160 160z" /></svg>
+                            <img src={down} alt="" />
                         </div>
                         :
                         <div className="moreBtn w-10 h-10 flex items-center justify-center absolute -bottom-2 shadow-md p-3 rounded-full bg-sandals cursor-pointer hover:scale-105 duration-150 opacity-0" style={{ left: "calc(50% - 1.25rem)" }} onClick={handleOpen}>
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="white" viewBox="0 0 448 512" style={{ transition: "300ms" }}><path d="M201.4 374.6c12.5 12.5 32.8 12.5 45.3 0l160-160c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 306.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l160 160z" /></svg>
+                            <img src={up} alt="" />
                         </div>
                     :
                     <></>
