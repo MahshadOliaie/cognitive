26e9bbbf08c3f1cdb784ab645cc7368aa5449686
MahshadOliaie <mahshadoliaie@gmail.com>
2024-08-27 11:43:51 +0330

    fix image post request

--- src/components/Modals/FileInput/FileInput.jsx
+++ src/components/Modals/FileInput/FileInput.jsx
@@ -6,8 +6,15 @@ function FileInput({ setImage, image, modalData }) {
     let fileKey = (modalData.id) ? modalData.original.fileKey : ""
 
 
-    function handleChange() {
-        fetch("http://cogcenter.ir/api/fs/v1/files", {
+
+
+    async function handleChange() {
+        const formData = new FormData();
+        formData.append("modelTypeId", 6);
+        formData.append("scopeId", 0);
+        formData.append("file", event.target.files[0]);
+
+        await fetch("https://cogcenter.ir/api/fs/v1/files", {
             method: "POST",
             headers: {
                 'accept': '*/*',
@@ -17,17 +24,13 @@ function FileInput({ setImage, image, modalData }) {
                 ],
                 "expiresIn": 1724266116069,
                 "refreshToken": "3eb183b8-340f-4452-af97-55015dd105b8",
-                'Content-Type': 'application/json'
             },
-            body: JSON.stringify({
-                'modelTypeId': "6",
-                'scopeId': "0",
-                'file': event.target.files[0]
-            }),
+            body: formData
         })
-            .then(setImage(event.target.files[0].name))
+            .then(res => res.json())
+            .then(data => setImage(data.fileName))
 
-        console.log(event.target.files[0])
+        console.log(image)
     }
 
 
@@ -36,9 +39,9 @@ function FileInput({ setImage, image, modalData }) {
             <div className="flex flex-col flex-1 border items-center justify-center bg-sand py-3.5 rounded-md shadow-inner max-w-52.5" style={{ borderColor: "lightgray" }}>
                 {(image) ?
                     (modalData.original?.coverImage) ?
-                        <label htmlFor="image" ><img src={`http://cogcenter.ir/api/fs/v1/files/download/${modalData.original.coverImage}?key=${fileKey}`} alt="" className="max-w-48" /></label>
+                        <label htmlFor="image" ><img src={`http://cogcenter.ir/api/fs/v1/files/download/${image}?key=${fileKey}`} alt="" className="max-w-48" /></label>
                         :
-                        <label htmlFor="image" >{image}</label>
+                        <label htmlFor="image" ><img src={`http://cogcenter.ir/api/fs/v1/files/download/${image}?key=${fileKey}`} alt="" className="max-w-48" /></label>
 
                     :
                     <label htmlFor="image" className="cursor-pointer opacity-70 text-sm p-5">انتخاب عکس</label>
