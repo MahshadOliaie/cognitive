de8d5e4b5b86b7a77d3fd2776c099e14c6762241
MahshadOliaie <mahshadoliaie@gmail.com>
2024-09-08 01:46:07 +0330

    edit overflow

--- src/components/Pages/AddBookPage/AuthorsNameTd.jsx
+++ src/components/Pages/AddBookPage/AuthorsNameTd.jsx
@@ -4,21 +4,21 @@ import { useState } from "react"
 function AuthorsNameTd({ props }) {
     const [showFullName, setShowFullName] = useState(false)
     return (
-        <>
-            <div className="relative flex">
-                <div className="overflow-x-scroll flex items-center justify-start flex-nowrap m-auto" style={{ maxWidth: "4.1rem" }} onMouseEnter={() => setShowFullName(true)} onMouseLeave={() => setShowFullName(false)}><p className="text-nowrap">{props.getValue()[0].firstName + " " + props.getValue()[0].lastName}</p>
+        <div className="relative">
+            <div className="relative flex overflow-hidden">
+                <div className="flex items-center justify-start flex-nowrap max-w-16" style={(props.getValue().length <= 11) ? {maxWidth: "5rem" , overflow: "hidden" } : {}} onMouseEnter={() => setShowFullName(true)} onMouseLeave={() => setShowFullName(false)}><p className="text-nowrap">{props.getValue()[0].firstName + " " + props.getValue()[0].lastName}</p>
                 </div>
-                {((props.getValue()[0].firstName.length) + (props.getValue()[0].lastName.length) > 10 || props.getValue().length > 1) &&
-                    <p>...</p>
-                }
-                {(showFullName && ((props.getValue()[0].firstName.length) + (props.getValue()[0].lastName.length) > 10 || props.getValue().length > 1)) &&
-                    <div className="absolute right-24 bg-white rounded-md shadow-sm p-3 z-30" style={{ display: "flex", alignItems: "center" }}>
+                {((props.getValue()[0].firstName.length) + (props.getValue()[0].lastName.length) > 11 || props.getValue().length > 1) &&
+                    <p className="absolute left-0">...</p>
+                }  
+            </div>
+            {(showFullName && ((props.getValue()[0].firstName.length) + (props.getValue()[0].lastName.length) > 11 || props.getValue().length > 1)) &&
+                    <div className="absolute right-24 bg-white rounded-md shadow-sm p-3 z-30" style={{ display: "flex", alignItems: "center" , top: "0" }}>
                         {props.getValue().map((author, index) => {
                             return <p className="text-nowrap" key={author.id}>{(index > 0) && "، "}{author.firstName + " " + author.lastName}</p>
                         })}
                     </div>}
-            </div>
-        </>
+        </div>
     )
 }
 
--- src/components/Pages/AddBookPage/BookNameTd.jsx
+++ src/components/Pages/AddBookPage/BookNameTd.jsx
@@ -4,17 +4,17 @@ import { useState } from "react"
 function BookNameTd({ props }) {
     const [showFullName, setShowFullName] = useState(false)
     return (
-        <>
-            <div className="relative flex">
-                <div className="overflow-x-scroll flex items-center justify-start flex-nowrap max-w-16 m-auto" onMouseEnter={() => setShowFullName(true)} onMouseLeave={() => setShowFullName(false)}>
+        <div className="relative">
+            <div className="relative flex overflow-hidden">
+                <div className="flex items-center justify-start flex-nowrap max-w-16" style={(props.getValue().length > 10) ? { maxWidth: "4.9rem", overflow: "hidden" } : {}} onMouseEnter={() => setShowFullName(true)} onMouseLeave={() => setShowFullName(false)}>
                     <p className="text-nowrap">{props.getValue()}</p>
                 </div>
                 {(props.getValue().length > 10) &&
-                    <p>...</p>
+                    <p className="absolute left-0">...</p>
                 }
-                {(showFullName && (props.getValue().length > 10)) && <div className="absolute right-24 bg-white rounded-md shadow-sm p-3 z-30"><p className="text-nowrap">{props.getValue()}</p></div>}
             </div>
-        </>
+            {(showFullName && (props.getValue().length > 10)) && <div className="absolute right-24 bg-white rounded-md shadow-sm p-3 z-40" style={{top: "0"}}><p className="text-nowrap">{props.getValue()}</p></div>}
+        </div>
     )
 }
 
