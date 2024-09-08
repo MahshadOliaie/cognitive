63d5e157884fbbd3a33e96794e4b174fd0d681c0
MahshadOliaie <mahshadoliaie@gmail.com>
2024-09-08 16:00:04 +0330

    edit overflow

--- src/components/Pages/AddBookPage/PublisherTd.jsx
+++ src/components/Pages/AddBookPage/PublisherTd.jsx
@@ -5,16 +5,16 @@ function PublisherTd({ props }) {
     const [showFullName, setShowFullName] = useState(false)
 
     return (
-        <>
-            <div className="relative flex">
-                <div className="overflow-x-scroll flex items-center justify-start flex-nowrap max-w-16 m-auto" onMouseEnter={() => setShowFullName(true)} onMouseLeave={() => setShowFullName(false)}><p className="text-nowrap">{props.getValue()}</p>
+        <div className="relative">
+            <div className="relative flex overflow-hidden">
+                <div className="flex items-center justify-start flex-nowrap max-w-16" style={(props.getValue().length >= 13) ? {maxWidth: "5rem" , overflow: "hidden" } : {}} onMouseEnter={() => setShowFullName(true)} onMouseLeave={() => setShowFullName(false)}><p className="text-nowrap">{props.getValue()}</p>
                 </div>
-                {(props.getValue().length > 12) &&
-                    <p>...</p>
+                {(props.getValue().length > 13) &&
+                    <p className="absolute left-0">...</p>
                 }
-                {(showFullName && (props.getValue().length > 12)) && <div className="absolute right-24 bg-white rounded-md shadow-sm p-3 z-30"><p className="text-nowrap">{props.getValue()}</p></div>}
             </div>
-        </>
+                {(showFullName && (props.getValue().length > 13)) && <div className="absolute right-24 bg-white rounded-md shadow-sm p-3 z-30 top-0"><p className="text-nowrap">{props.getValue()}</p></div>}
+        </div>
     )
 }
 
